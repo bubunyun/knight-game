0c7873a2cea757ac5315a1b34eea1452583cbe24
bubunyun <171100337+bubunyun@users.noreply.github.com>
2025-04-13 22:26:30 +0100

    completed

---
+++ scripts/coin.gd
@@ -0,0 +1,13 @@
+class_name Coin
+extends Area2D
+
+
+@onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
+	animation_player.play("pickup")
+	if name == "LastCoin":
+		game_manager.end_game()
---
+++ scripts/game_manager.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+var score = 0
+@onready var score_label: Label = $ScoreLabel
+@onready var player: CharacterBody2D = %Player
+@onready var timer: Timer = $Timer
+
+
+func add_point():
+	score += 1
+	score_label.text = "you collected " + str(score) + " coins"
+
+	
+func end_game():
+	Engine.time_scale = 0.5
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
+
+
+func _on_last_coin_collected() -> void:
+	end_game()
